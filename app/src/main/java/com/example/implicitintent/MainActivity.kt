e52b16e8b6e340715a11cfb9c4a2dfd2d26dc26d
akash dev <akash.dev.8092@gmail.com>
2023-03-20 07:19:42 +0530

    added apk files

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webButton = findViewById<CardView>(R.id.chrome)
+        val camButton = findViewById<CardView>(R.id.camera)
+        val callButton = findViewById<CardView>(R.id.phone)
+
+        webButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("http://www.google.com")
+            startActivity(intent)
+        }
+
+        camButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+
+        }
+        callButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
+    }
+}
